b2871aec878338190244148198515f60b2e98f37
unamednada <dias.gustavo.dias@gmail.com>
2021-10-27 16:02:58 -0300

    Start work on redux and main jsx

---
+++ src/actions/index.js
@@ -0,0 +1,33 @@
+export const REQUEST = 'REQUEST';
+export const SUCCESS = 'SUCCESS';
+export const FAIL = 'FAIL';
+export const SELECT = 'SELECT';
+
+export const select = (selected) => ({
+  type: SELECT,
+  selected,
+})
+export const request = () => ({
+  type: REQUEST,
+});
+
+export const success = (json) => ({
+  type: SUCCESS,
+  json,
+});
+
+export const fail = (error) => ({
+  type: FAIL,
+  error,
+});
+
+export const fetchPosts = (subReddit) => {
+  return (dispatch) => {
+    dispatch(request());
+    return fetch(`https://www.reddit.com/r/${subReddit}.json`)
+      .then(
+        (json) => dispatch(success(json)),
+        (error) => dispatch(fail(error)),
+      )
+  }
+} 
---
+++ src/reducers/index.js
@@ -0,0 +1,43 @@
+import { combineReducers } from "redux";
+import { REQUEST, SUCCESS, FAIL, SELECT } from '../actions';
+
+const INITIAL_STATE = {
+  selected: '',
+  loading: false,
+  json: {},
+  error: null,
+};
+
+const defaultReducer = (state = INITIAL_STATE, action) => {
+  switch(action.type) {
+    case SELECT:
+      return {
+        ...state,
+        selected: action.selected,
+      }
+    case REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case SUCCESS:
+      return {
+        ...state,
+        json: action.json,
+        loading: false,
+      };
+    case FAIL:
+      return {
+        ...state,
+        error: action.error,
+        loading: false,
+      }
+    default:
+      return state;
+  }
+}
+
+
+const rootReducer = combineReducers({ defaultReducer });
+
+export default rootReducer;
